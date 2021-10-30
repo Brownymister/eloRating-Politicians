2507fc66ec530aad18730693c031c69c7dc67219
Browny <81575762+Brownymister@users.noreply.github.com>
2021-10-30 22:11:17 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+var Ra = 1000;
+var Rb = 1000;
+
+app.listen(4000);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/Ra', function(req, res) {
+    RaWin();
+    res.json({
+        "elo": {
+            "Ra": Ra,
+            "Rb": Rb,
+        }
+    });
+});
+
+app.get('/Rb', function(req, res) {
+    RbWin();
+    res.json({
+        "elo": {
+            "Ra": Ra,
+            "Rb": Rb,
+        }
+    });
+});
+
+app.get('/getELO', function(req, res) {
+    res.json({
+        "elo": {
+            "Ra": Ra,
+            "Rb": Rb,
+        }
+    });
+});
+
+app.get('/result', function(req, res) {
+    res.sendFile(__dirname + '/result.html');
+});
+
+app.get('/getImg', function(req, res) {
+    res.json({
+        "Img": {
+            "Ra": 'https://pbs.twimg.com/media/E87IXSYWQAI-8ti?format=jpg&name=large',
+            "Rb": 'https://www.filmfestivalcottbus.de/media/k2/items/cache/2f7bb693f9e8b1bbf875f0818996978d_XL.jpg',
+        }
+    });
+});
+
+function RaWin() {
+    Ea = 1 / (1 + Math.pow(10, ((Rb - Ra) / 400)))
+    Eb = 1 / (1 + Math.pow(10, ((Ra - Rb) / 400)))
+    Ra = Ra + 32 * (1 - Ea)
+    Rb = Rb + 32 * (0 - Eb)
+}
+
+function RbWin() {
+    Ea = 1 / (1 + Math.pow(10, ((Rb - Ra) / 400)))
+    Eb = 1 / (1 + Math.pow(10, ((Ra - Rb) / 400)))
+    Ra = Ra + 32 * (0 - Ea)
+    Rb = Rb + 32 * (1 - Eb)
+}
